4e70bf72d76508eb4544d2b30b86407e032f7e32
SUNYUHA0 <127923011+SUNYUHA0@users.noreply.github.com>
2023-03-17 22:08:10 +0800

    Update hexagons.py

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,3 +1,6 @@
+'''
+Substitute the numbers from 1 to 5 into the equation and output the result
+'''
 for i in range(1,6): #to input i from 1 to 6
  print(i*(2*i-1),end=" ") #Output calculated value #end=' ' causes the output value to be printed on a single line, separated by spaces.
 # answer is 1 6 15 28 45
