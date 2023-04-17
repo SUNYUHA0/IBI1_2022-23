1d6fe721fc10c9dea07cecbe8331a24835eda6e0
SUNYUHA0 <127923011+SUNYUHA0@users.noreply.github.com>
2023-04-17 20:11:21 +0800

    Delete cds_counter.py

--- Practtical9/cds_counter.py
+++
@@ -1,8 +0,0 @@
-import re
-seq = 'ATGCAATCGACTACGATCTGAGAGGGCCTAA'
-x = re.findall(r'^ATG.+', seq)  # Find the first sequence in which "ATG" appears
-y = "".join(x)  # change the list into string.
-a = y.count('TGA')
-b = y.count('TAA')
-c = a + b
-print(c)  # Print the total	number of possible coding sequences	formed	by this sequence.
