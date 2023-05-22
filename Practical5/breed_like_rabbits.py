70e287ec88032a0b95ca323964793f97b0c701cb
SUNYUHA0 <127923011+SUNYUHA0@users.noreply.github.com>
2023-05-22 13:15:44 +0800

    Update breed_like_rabbits.py

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -1,10 +1,10 @@
 '''
 When the number of rabbits is less than 100, double the number and add the number of generations until the number of rabbits is greater than one hundred
 '''
-x = 2 # the number of ducks in first generation
+x = 2 # the number of rabbits in first generation
 y = 1 # the first generation
-while x < 100: #When the total number of ducks is less than 100
- y+=1 # the number of generation plus 1
+while x < 100: #When the total number of rabbits is less than 100
+ y += 1 # the number of generation plus 1
  x = 2*x # Double the number of ducks
-if x > 100:When the total number of ducks is greater than 100
- print(y) # output result
+if x > 100:When the total number of rabbits is greater than 100
+ print(y) # output result. At 7 generation over 100 rabbits have been born.
