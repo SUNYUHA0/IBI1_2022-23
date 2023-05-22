6a7037ac5aefb5cbf5b966111db6134ad2b069da
SUNYUHA0 <127923011+SUNYUHA0@users.noreply.github.com>
2023-05-22 13:11:11 +0800

    Update mystery_code.py

--- Practical5/mystery_code.py
+++ Practical5/mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer:
+# Answer: the output is the largest of ten randomly generated numbers from 1 to 100
 
 # Import libraries
 # randint allows drawing a random number,
@@ -10,10 +10,10 @@ from random import randint
 # e.g. ceil(4.2)=5
 from math import ceil
 
-progress=0
-stored_random_number=0
-while progress<10:
-	progress+=1 #The cycle time is ten times
+progress = 0
+stored_random_number = 0
+while progress < 10:
+	progress += 1 #The cycle time is ten times
 	n = randint(1,100) #Pick a random number between 1 and 100
 	if n > stored_random_number:
 		stored_random_number = n #If n is large, then n is chosen
