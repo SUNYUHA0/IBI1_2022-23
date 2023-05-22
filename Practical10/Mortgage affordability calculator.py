e946192fe808dbf020eb260a14a831c26a047b11
SUNYUHA0 <127923011+SUNYUHA0@users.noreply.github.com>
2023-05-22 18:51:09 +0800

    Update Mortgage affordability calculator.py

--- Practical10/Mortgage affordability calculator.py
+++ Practical10/Mortgage affordability calculator.py
@@ -2,14 +2,14 @@
 Compare the size of value to salary
 '''
 def calculator(value, salary):
-    if value > 5*salary:
+    if value >= 5*salary:
         return "No"
     else:
         return "Yes"
 
 # value = 180000
 # salary = 35000
-value = 100000
-salary = 9000
+value = 180000
+salary = 35000
 z = calculator(value, salary)
-print(calculator(value,salary))  # An example.
+print(calculator(value,salary))  # The anwser is No.
