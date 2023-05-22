6a9adf34ef4e9ec3aa66e83f867f73cc05a6ea16
SUNYUHA0 <127923011+SUNYUHA0@users.noreply.github.com>
2023-05-22 19:24:33 +0800

    Update Protein-coding capability calculator.py

--- Practical10/Protein-coding capability calculator.py
+++ Practical10/Protein-coding capability calculator.py
@@ -2,16 +2,21 @@
 Calculate the percentage and define the name
 '''
 import re
-def calculator (seq):
+def calculator(seq):
     seq = seq.upper()  # Convert lowercase to uppercase
-    a = re.findall(r'^ATG.+?TGA', seq)  # Take all sequences from the beginning of ATG to the end of TGA
-    b = len(a[0]) / len(seq)  # Calculated percentage
+    a = re.findall(r'ATG.+?TGA', seq)  # Take all sequences from the beginning of ATG to the end of TGA
+    b = (len(a[0]) - 3) / len(seq)  # Since the stop codon does not encode an amino acid, the total length needs to be subtracted from the length of the stop codon, 3. Then calculate the percentage..
     if b > 0.5:
         return str('percent: {:.2%}'.format(b)) + ' ' + 'protein-coding'
     elif b < 0.1:
         return str('percent: {:.2%}'.format(b)) + ' ' + 'non-coding'
-    else: str('percent: {:.2%}'.format(b)) + ' ' + 'unclear'
+    else:
+        return str('percent: {:.2%}'.format(b)) + ' ' + 'unclear'
     
-seq = 'ATGTGCACATtgagtaaac'
+    
+seq = input("Please input a DNA sequence:")
 z = calculator(seq)
 print(calculator(seq))
+  # seq = 'ATGTGCAtgagtaaacattg'. And the output is 'percent: 35.00% unclear'.
+  # seq = 'AAAAAAAATTTTTTATGCCCCGGGGGAAATTTTTGAAAAAAA'. And the output is 'percent: 45.24% unclear'.
+  # seq = 'TTGATGCCGTCTGGCTGATTT'. And the outpot is 'percent: 57.14% protein-coding'.
