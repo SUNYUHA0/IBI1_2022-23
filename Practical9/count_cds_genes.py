9daad9f6511985f31c677023cd91787f6316163a
SUNYUHA0 <127923011+SUNYUHA0@users.noreply.github.com>
2023-05-22 16:08:13 +0800

    Update count_cds_genes.py

--- Practical9/count_cds_genes.py
+++ Practical9/count_cds_genes.py
@@ -1,12 +1,13 @@
 import re
-Input = input('please input the stop codon like TAA, TAG and TGA')
+Input = input('Please input TAA, TAG and TGA as the stop codon: ')
 seq = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r')
-new_file = open('%s_stop_genes.fa' % Input, 'w')
+new_file = open('%s_stop_genes.fa' % Input, 'w')  # make a file including input
 a = seq.read()
 b = a.split('>')  # Split the sequence of each gene
 k = 0
 for i in b:
-    if re.findall(Input+r'n$',i):  # Find the sequence that ends in the Input and newlines
+    if re.findall(Input+r'\n$', i):  # Find the sequence that ends in the Input and newlines
         c = re.sub(r'\n', '', b[k])  # Remove all newlines
-        new_file.write('>' + ''.join(re.findall(r' gene:(.+?) ', c)) + ' ' + str(len(re.findall(Input, c))) + '\n' + ''.join(re.findall(r'](.+)', c)) + '\n')  # the number of coding sequences which could	be generated using the given stop codon
-    k += 1
+        new_file.write('>' + ''.join(re.findall(r' gene:(.+?) ', c)) + ' ' + str(len(re.findall(Input, c))) + '\n' + ''.join(re.findall(r'](.+)', c)) + '\n')
+        # the number of coding sequences which could	be generated using the given stop codon
+    k += 1  # Record the serial number in the list
