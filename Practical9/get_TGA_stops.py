cab99d0ca7343a3753db8b1bd6c0df0f404f32c4
SUNYUHA0 <127923011+SUNYUHA0@users.noreply.github.com>
2023-05-22 15:32:01 +0800

    Update get_TGA_stops.py

--- Practical9/get_TGA_stops.py
+++ Practical9/get_TGA_stops.py
@@ -8,4 +8,4 @@ for i in b:
     if re.findall(r'TGA\n$', i):  # Find the sequence that ends in TGA
         c = re.sub(r'\n', '', b[k])  # Remove all newlines
         new_file.write('>' + ''.join(re.findall(r' gene:(.+?) ', c)) + '\n' + ''.join(re.findall(r'](.+)', c)) + '\n')  # Put the the name and the sequence
-    k += 1
+    k += 1  # Record the serial number in the list
